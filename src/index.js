b9029ac0e0e21fb18328d08445cd9f43545682dc
orange <orange>
2022-08-17 15:32:49 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+function component() {
+  var element = document.createElement("div");
+  element.innerHTML = "hello";
+  return element;
+}
+
+document.body.appendChild(component());
